5e26ad1ad81c88fad807eb4e466b298d33fad88a
Jennyf19 <ferriesj@gmail.com>
2016-07-26 15:07:54 -0700

    first commit for PSET1 Smart Water

--- CS50Pset1.Water/Program.cs
+++ CS50Pset1.Water/Program.cs
@@ -8,8 +8,21 @@ namespace CS50Pset1.Water
 {
     class Program
     {
+        private static int numberOfBottles;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("How long is your shower in minutes?");
+            string input = Console.ReadLine();
+            int i;
+            Bottles myBottles = new Bottles(input, out i);
+            int showerLength;
+            Int32.TryParse(input, out showerLength);
+
+            Console.WriteLine("Minutes: {0} Bottles: {1}", showerLength, numberOfBottles);
+            Console.ReadLine();
+
+
         }
     }
 }
---
+++ CS50Pset1.Water/bottles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS50Pset1.Water
+{
+    class Bottles
+    {
+        
+        public int numberOfBottles;
+        public int showerLength;
+
+        public Bottles(string minutes, out int showerLength)
+        {
+            showerLength = Int32.Parse(minutes);
+        }
+
+        #region Methods
+
+        public int Shower(int showerLength)
+        {
+            if (showerLength <= 0)
+            {
+                Console.WriteLine("Please enter a value greater than 0");
+            }
+           else  numberOfBottles = (showerLength*12);
+            return numberOfBottles;
+            
+        }
+
+        #endregion
+
+    }
+}
